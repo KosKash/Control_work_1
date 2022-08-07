e5de1821a37e84be88621133ad823ab7a2acc9b0
Constantine <koskash98ksband@gmail.com>
2022-08-07 16:38:08 +0600

    Добавлен модуль нового массива

--- CW_Prog/Program.cs
+++ CW_Prog/Program.cs
@@ -5,3 +5,11 @@ int elemCount = 0;
 for (int i = 0; i < inputArray.Length; i++)
     if (inputArray[i].Length <= 3) elemCount++;
 
+string[] outArr = new string[elemCount];
+int index = 0;
+for (int i = 0; i < inputArray.Length; i++)
+    if (inputArray[i].Length < 4)
+    {
+        outArr[index] = inputArray[i];
+        index++;
+    }
