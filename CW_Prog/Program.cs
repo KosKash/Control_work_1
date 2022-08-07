aebc0415346ea9faaed970f6e6fcf1bef7914779
Constantine <koskash98ksband@gmail.com>
2022-08-07 16:42:31 +0600

    Добавлены коментарии

--- CW_Prog/Program.cs
+++ CW_Prog/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.Clear();
 string[] inputArray = new string[]
 {"one","two","three", "four", "1","3","2", "job"};
-int elemCount = 0;
+
+int elemCount = 0;  // Блок который считает количество нужных элементов для инициализации нового массива
 for (int i = 0; i < inputArray.Length; i++)
     if (inputArray[i].Length <= 3) elemCount++;
 
-string[] outArr = new string[elemCount];
+string[] outArr = new string[elemCount];  //Этот блок отвечает за запись элемента массива в новый массив удовлетворя требованиям
 int index = 0;
 for (int i = 0; i < inputArray.Length; i++)
     if (inputArray[i].Length < 4)
@@ -14,13 +15,12 @@ for (int i = 0; i < inputArray.Length; i++)
         index++;
     }
 
-void PrintArray(string[] array)
+void PrintArray(string[] array) // Метод который печатает массив в консоль
 {
     for (int i = 0; i < array.Length; i++)
     {
         System.Console.Write($"{array[i]} ");
     }
 }
-
 PrintArray(outArr);
 Console.ReadKey();
