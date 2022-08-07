4b404d37f5411212155f93b72708c1fa3f1fe13d
Constantine <koskash98ksband@gmail.com>
2022-08-07 16:36:54 +0600

    Добавлен счетчик элементов массива где менее 3 символов

---
+++ CW_Prog/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+string[] inputArray = new string[]
+{"one","two","three", "four", "1","3","2", "job"};
+int elemCount = 0;
+for (int i = 0; i < inputArray.Length; i++)
+    if (inputArray[i].Length <= 3) elemCount++;
+
